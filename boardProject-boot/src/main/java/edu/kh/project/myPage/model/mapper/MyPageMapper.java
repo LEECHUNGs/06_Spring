1c0e7e70acf5e3d9194473a5971ff4d8e82e1bbf
LEECHUNGs <ragnacrimson@naver.com>
2024-04-17 17:33:39 +0900

    Spring boardProject profile

--- boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/mapper/MyPageMapper.java
@@ -1,10 +1,12 @@
 package edu.kh.project.myPage.model.mapper;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
 import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.myPage.model.dto.UploadFile;
 
 @Mapper
 public interface MyPageMapper {
@@ -27,6 +29,27 @@ public interface MyPageMapper {
 	 */
 	int changePw(Map<String, Object> paramMap);
 
+	/** 회원 탈퇴
+	 * @param memberNo
+	 * @return result
+	 */
 	int secession(int memberNo);
+
+	/** 파일 정보를 DB 에 삽입
+	 * @param uf
+	 * @return result
+	 */
+	int insertUploadFile(UploadFile uf);
+
+	/** 파일 목록 조회
+	 * @return list
+	 */
+	List<UploadFile> fileList();
+
+	/** 프로필 이미지 변경
+	 * @param member
+	 * @return
+	 */
+	int profile(Member member);
 
 }
--- boardProject-boot/src/main/java/edu/kh/project/myPage/model/service/MyPageServiceImpl.java
+++ boardProject-boot/src/main/java/edu/kh/project/myPage/model/service/MyPageServiceImpl.java
@@ -1,20 +1,29 @@
 package edu.kh.project.myPage.model.service;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import edu.kh.project.common.util.Utility;
 import edu.kh.project.member.model.dto.Member;
+import edu.kh.project.myPage.model.dto.UploadFile;
 import edu.kh.project.myPage.model.mapper.MyPageMapper;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(rollbackFor = Exception.class) // 모든 예외시 롤백 
 @RequiredArgsConstructor
+@PropertySource("classpath:/config.properties")
 public class MyPageServiceImpl implements MyPageService{
 	
 	private final MyPageMapper mapper;
@@ -22,6 +31,13 @@ public class MyPageServiceImpl implements MyPageService{
 	// BCrypt 암호화 객체 의존성 주입(SecureityConfig 참고)
 	private final BCryptPasswordEncoder bcrypt;
 
+	// 
+	@Value("${my.profile.web-path}")
+	private String profileWebPath;
+	
+	@Value("${my.profile.folder-path}")
+	private String profileFolderPath;
+	
 	// 회원 정보 수정
 	@Override
 	public int updateInfo(Member inputMember, String[] memberAddress) {
@@ -111,4 +127,166 @@ public class MyPageServiceImpl implements MyPageService{
 		
 		return "/myPage/file/" + uploadFile.getOriginalFilename();
 	}
+
+	@Override
+	public int fileUpload2(MultipartFile uploadFile, int memberNo) throws Exception{
+		
+		// 업로드된 파일이 없다면
+		// == 선택된 파일이 없을 경우
+		if(uploadFile.isEmpty()) {
+			return 0;
+		}
+		
+		
+		/* DB에 파일 저장이 가능은 하나,
+		 * DB의 부하를 줄이기 위해
+		 * 
+		 * 1) DB에는 서버에 저장할 파일 경로를 저장
+		 * 
+		 * 2) DB 삽입/수정 성공 후 서버에 파일을 저장
+		 * 
+		 * 3) 만약 파일 저장 실패 시
+		 *    -> 예외 발생
+		 *    -> @Transactional 을 이용해 rollback 수행
+		 *    
+		 * */
+		
+		// 1. 서버에 저장할 파일 경로 만들기
+		
+		// 파일이 저장될 서버 폴더 경로
+		String folderPath = "C:/uploadFiles/test/";
+		
+		// 클라이언트가 파일이 저장된 폴더에 접근할 수 있는 주소
+		String webPath = "/myPage/file/";
+		
+		// 2. DB에 전달할 데이터를 DTO로 묶어 INSERT 호출
+		// webPath, memberNo, 원본 파일명, 변경된 파일명
+		String fileRename = Utility.fileRename(uploadFile.getOriginalFilename());
+		
+		// Builder 패턴을 사용해 UploadFile 객체 생성
+		// 장점 1) 반복되는 참조변수명, set 구문 생략
+		// 장점 2) method chaining 을 이용해 한 줄로 작성 가능
+		UploadFile uf = UploadFile.builder()
+						.memberNo(memberNo)
+						.filePath(webPath)
+						.fileOriginalName(uploadFile.getOriginalFilename())
+						.fileRename(fileRename)
+						.build();
+		
+		int result = mapper.insertUploadFile(uf);
+		
+		// 3. 삽입 (INSERT) 성공 시 파일을 지정된 서버 폴더에 저장
+		
+		// 삽입 실패 시
+		if(result == 0) return 0;
+		
+		// C:/uploadFiles/test/변경된파일명 으로
+		// 파일을 서버 컴퓨터에 저장
+		
+		uploadFile.transferTo(new File(folderPath + fileRename));
+		
+		// @Transactional은 RuntimException(UncheckedException 의 대표 예시) 만 처리
+		// -> rollbackFor 속성 이용해
+		//롤백할 예외 범위를 수정
+		
+		return result;
+	}
+
+	@Override
+	public List<UploadFile> fileList() {
+		
+		return mapper.fileList();
+	}
+
+	// 여러 파일 업로드
+	@Override
+	public int fileUpload3(List<MultipartFile> aaaList, List<MultipartFile> bbbList, int memberNo) throws Exception{
+		
+		// 1. aaaList 처리
+		int result1 = 0;
+		
+		// 업로드된 파일이 없을 경우를 제외하고 업로드
+		for(MultipartFile file : aaaList) {
+			
+			if(file.isEmpty()) continue;
+			
+			// fileUpload2() 메서드 호출 (재활용)
+			// -> 파일 하나 업로드 + DB INSERT
+			result1 += fileUpload2(file, memberNo);
+		}
+		
+		// 2. bbbList 처리
+		int result2 = 0;
+		
+		for(MultipartFile file : bbbList) {
+			
+			if(file.isEmpty()) continue;
+			
+			// fileUpload2() 메서드 호출 (재활용)
+			// -> 파일 하나 업로드 + DB INSERT
+			result2 += fileUpload2(file, memberNo);
+		}
+		
+		return result1 + result2;
+	}
+
+	@Override
+	public int profile(MultipartFile profileImg, Member loginMember) throws Exception{
+		
+		// 수정할 경로
+		String updatePath = null;
+		
+		// 변경명 저장
+		String rename = null;
+		
+		// 업로드한 이미지가 있을 경우
+		// - 있을 경우 : 수정할 경로 조합 (클라이언트 접근 경로 + 리네임 파일명)
+		if( !profileImg.isEmpty() ) {
+			// updatPath 조합
+			
+			// 1. 파일명 변경
+			rename = Utility.fileRename(profileImg.getOriginalFilename());
+			
+			// 2. /myPage/profile/변경된 파일명
+			updatePath = profileWebPath + rename;
+		}
+		
+		// 수정된 프로필 이미지 경로 + 회원 번호 DTO 객체 
+		Member member = Member.builder()
+						.memberNo(loginMember.getMemberNo())
+						.profileImg(updatePath)
+						.build();
+		
+		// UPDATE 수행
+		int result = mapper.profile(member);
+		
+		if(result > 0) { // DB에 수정 성공 시
+			 
+			// 프로필 이미지를 없엔 경우 (NULL 로 수정한 경우) 를 제외
+			// -> 업로드한 이미지가 있을 경우
+			if( !profileImg.isEmpty() ) {
+				// 파일을 서버 지정된 폴더에 저장
+				profileImg.transferTo(new File(profileFolderPath + rename));
+			}
+			
+			// 세션 회원 정보에서 프로필 이미지 경로를
+			// 업데이트한 경로로 변경
+			loginMember.setProfileImg(updatePath);
+		}
+		
+		return result;
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
